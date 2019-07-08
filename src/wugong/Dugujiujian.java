aafa7cfbd202f0f06bbd3fc28442fc4285ebabca
linghuchong <949387321@qq.com>
2019-07-08 20:25:54 +0800

    yyyyyyy

--- src/wugong/Dugujiujian.java
+++ src/wugong/Dugujiujian.java
@@ -4,5 +4,9 @@ public class Dugujiujian {
      public static void main(String[] args) {
 		System.out.println("111111111");
 		System.out.println("222222222");
+		System.out.println("333333333");
+		System.out.println("444444444");
+		System.out.println("555555555");
+		System.out.println("yyyyyyyyy");
 	}
 }
